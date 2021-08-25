1339dc1d5afa6a27195d0fb1d64e3f992f3fbdd8
Hariharan0496 <84595066+Hariharan0496@users.noreply.github.com>
2021-08-25 20:15:42 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,11 @@
+var resume=[{"Name":"Hariharan.U.S",
+             "qualification":"B.E",
+             "email":"hari@gmail.com",
+            "Experience":"fresher",
+        "Address":"abc",
+    "Domain":"Full stack development",}];
+ //console.log(resume);
+resume.forEach(function() {
+    console.log(resume);
+});
+  
